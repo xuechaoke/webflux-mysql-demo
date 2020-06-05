793efdd90ff04a6529ccce73145d2676d8ec3245
ciao <xuechaoke1995@gmail.com>
2020-06-05 15:57:14 +0800

    新增  全局唯一id 配置  使用uuid 作为ID

--- src/main/java/com/ciao/webfluxmysqldemo/WebfluxMysqlDemoApplication.java
+++ src/main/java/com/ciao/webfluxmysqldemo/WebfluxMysqlDemoApplication.java
@@ -1,9 +1,12 @@
 package com.ciao.webfluxmysqldemo;
 
+import com.ciao.webfluxmysqldemo.repository.DefaultR2dbcRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@EnableR2dbcRepositories(repositoryBaseClass = DefaultR2dbcRepository.class)
 public class WebfluxMysqlDemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/ciao/webfluxmysqldemo/repository/DefaultR2dbcRepository.java
@@ -0,0 +1,304 @@
+/*
+ * Copyright 2018-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ciao.webfluxmysqldemo.repository;
+
+import cn.hutool.core.bean.BeanUtil;
+import org.reactivestreams.Publisher;
+import org.springframework.data.r2dbc.convert.R2dbcConverter;
+import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.data.r2dbc.core.R2dbcEntityOperations;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.r2dbc.core.ReactiveDataAccessStrategy;
+import org.springframework.data.relational.core.mapping.RelationalPersistentProperty;
+import org.springframework.data.relational.core.query.Criteria;
+import org.springframework.data.relational.core.query.Query;
+import org.springframework.data.relational.repository.query.RelationalEntityInformation;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.util.Lazy;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static com.ciao.webfluxmysqldemo.util.IdGenerator.generateUUID;
+
+/**
+ * Simple {@link ReactiveCrudRepository} implementation using R2DBC through {@link DatabaseClient}.
+ *
+ * @author Mark Paluch
+ * @author Jens Schauder
+ * @author Mingyuan Wu
+ */
+@Transactional(readOnly = true)
+public class DefaultR2dbcRepository<T, ID> implements ReactiveCrudRepository<T, ID> {
+
+    private final RelationalEntityInformation<T, ID> entity;
+    private final R2dbcEntityOperations entityOperations;
+    private final Lazy<RelationalPersistentProperty> idProperty;
+
+    /**
+     * Create a new {@link DefaultR2dbcRepository}.
+     *
+     * @param entity
+     * @param entityOperations
+     * @param converter
+     * @since 1.1
+     */
+    public DefaultR2dbcRepository(RelationalEntityInformation<T, ID> entity, R2dbcEntityOperations entityOperations,
+                                  R2dbcConverter converter) {
+
+        this.entity = entity;
+        this.entityOperations = entityOperations;
+        this.idProperty = Lazy.of(() -> converter //
+                .getMappingContext() //
+                .getRequiredPersistentEntity(this.entity.getJavaType()) //
+                .getRequiredIdProperty());
+    }
+
+    /**
+     * Create a new {@link DefaultR2dbcRepository}.
+     *
+     * @param entity
+     * @param databaseClient
+     * @param converter
+     * @param accessStrategy
+     */
+    public DefaultR2dbcRepository(RelationalEntityInformation<T, ID> entity, DatabaseClient databaseClient,
+                                  R2dbcConverter converter, ReactiveDataAccessStrategy accessStrategy) {
+
+        this.entity = entity;
+        this.entityOperations = new R2dbcEntityTemplate(databaseClient);
+        this.idProperty = Lazy.of(() -> converter //
+                .getMappingContext() //
+                .getRequiredPersistentEntity(this.entity.getJavaType()) //
+                .getRequiredIdProperty());
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#save(S)
+     */
+    @Override
+    @Transactional
+    public <S extends T> Mono<S> save(S objectToSave) {
+
+        Assert.notNull(objectToSave, "Object to save must not be null!");
+
+        if (this.entity.isNew(objectToSave)) {
+            BeanUtil.setFieldValue(objectToSave, "id", generateUUID());
+            return this.entityOperations.insert(objectToSave);
+        }
+
+        return this.entityOperations.update(objectToSave);
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#saveAll(java.lang.Iterable)
+     */
+    @Override
+    @Transactional
+    public <S extends T> Flux<S> saveAll(Iterable<S> objectsToSave) {
+
+        Assert.notNull(objectsToSave, "Objects to save must not be null!");
+
+        return Flux.fromIterable(objectsToSave).concatMap(this::save);
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#saveAll(org.reactivestreams.Publisher)
+     */
+    @Override
+    @Transactional
+    public <S extends T> Flux<S> saveAll(Publisher<S> objectsToSave) {
+
+        Assert.notNull(objectsToSave, "Object publisher must not be null!");
+
+        return Flux.from(objectsToSave).concatMap(this::save);
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#findById(java.lang.Object)
+     */
+    @Override
+    public Mono<T> findById(ID id) {
+
+        Assert.notNull(id, "Id must not be null!");
+
+        return this.entityOperations.selectOne(getIdQuery(id), this.entity.getJavaType());
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#findById(org.reactivestreams.Publisher)
+     */
+    @Override
+    public Mono<T> findById(Publisher<ID> publisher) {
+        return Mono.from(publisher).flatMap(this::findById);
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#existsById(java.lang.Object)
+     */
+    @Override
+    public Mono<Boolean> existsById(ID id) {
+
+        Assert.notNull(id, "Id must not be null!");
+
+        return this.entityOperations.exists(getIdQuery(id), this.entity.getJavaType());
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#existsById(org.reactivestreams.Publisher)
+     */
+    @Override
+    public Mono<Boolean> existsById(Publisher<ID> publisher) {
+        return Mono.from(publisher).flatMap(this::findById).hasElement();
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#findAll()
+     */
+    @Override
+    public Flux<T> findAll() {
+        return this.entityOperations.select(Query.empty(), this.entity.getJavaType());
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#findAllById(java.lang.Iterable)
+     */
+    @Override
+    public Flux<T> findAllById(Iterable<ID> iterable) {
+
+        Assert.notNull(iterable, "The iterable of Id's must not be null!");
+
+        return findAllById(Flux.fromIterable(iterable));
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#findAllById(org.reactivestreams.Publisher)
+     */
+    @Override
+    public Flux<T> findAllById(Publisher<ID> idPublisher) {
+
+        Assert.notNull(idPublisher, "The Id Publisher must not be null!");
+
+        return Flux.from(idPublisher).buffer().filter(ids -> !ids.isEmpty()).concatMap(ids -> {
+
+            if (ids.isEmpty()) {
+                return Flux.empty();
+            }
+
+            String idProperty = getIdProperty().getName();
+
+            return this.entityOperations.select(Query.query(Criteria.where(idProperty).in(ids)), this.entity.getJavaType());
+        });
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#count()
+     */
+    @Override
+    public Mono<Long> count() {
+        return this.entityOperations.count(Query.empty(), this.entity.getJavaType());
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#deleteById(java.lang.Object)
+     */
+    @Override
+    @Transactional
+    public Mono<Void> deleteById(ID id) {
+
+        Assert.notNull(id, "Id must not be null!");
+
+        return this.entityOperations.delete(getIdQuery(id), this.entity.getJavaType()).then();
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#deleteById(org.reactivestreams.Publisher)
+     */
+    @Override
+    @Transactional
+    public Mono<Void> deleteById(Publisher<ID> idPublisher) {
+
+        Assert.notNull(idPublisher, "The Id Publisher must not be null!");
+
+        return Flux.from(idPublisher).buffer().filter(ids -> !ids.isEmpty()).concatMap(ids -> {
+
+            if (ids.isEmpty()) {
+                return Flux.empty();
+            }
+
+            String idProperty = getIdProperty().getName();
+
+            return this.entityOperations.delete(Query.query(Criteria.where(idProperty).in(ids)), this.entity.getJavaType());
+        }).then();
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#delete(java.lang.Object)
+     */
+    @Override
+    @Transactional
+    public Mono<Void> delete(T objectToDelete) {
+
+        Assert.notNull(objectToDelete, "Object to delete must not be null!");
+
+        return deleteById(this.entity.getRequiredId(objectToDelete));
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#deleteAll(java.lang.Iterable)
+     */
+    @Override
+    @Transactional
+    public Mono<Void> deleteAll(Iterable<? extends T> iterable) {
+
+        Assert.notNull(iterable, "The iterable of Id's must not be null!");
+
+        return deleteAll(Flux.fromIterable(iterable));
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#deleteAll(org.reactivestreams.Publisher)
+     */
+    @Override
+    @Transactional
+    public Mono<Void> deleteAll(Publisher<? extends T> objectPublisher) {
+
+        Assert.notNull(objectPublisher, "The Object Publisher must not be null!");
+
+        Flux<ID> idPublisher = Flux.from(objectPublisher) //
+                .map(this.entity::getRequiredId);
+
+        return deleteById(idPublisher);
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.data.repository.reactive.ReactiveCrudRepository#deleteAll()
+     */
+    @Override
+    @Transactional
+    public Mono<Void> deleteAll() {
+        return this.entityOperations.delete(Query.empty(), this.entity.getJavaType()).then();
+    }
+
+    private RelationalPersistentProperty getIdProperty() {
+        return this.idProperty.get();
+    }
+
+    private Query getIdQuery(Object id) {
+        return Query.query(Criteria.where(getIdProperty().getName()).is(id));
+    }
+}
